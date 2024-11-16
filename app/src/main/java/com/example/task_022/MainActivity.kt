52bb2473035cd6185175a6dd7bc1cb11ce952eb1
Aleksandr Romanets <a.romanets@yandex.ru>
2024-11-16 13:03:14 +0300

    second commit

--- app/src/main/java/com/example/task_022/MainActivity.kt
+++ app/src/main/java/com/example/task_022/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
@@ -21,7 +22,10 @@ class MainActivity : AppCompatActivity() {
     private var personLastName = ""
     private var personAge = ""
     private var personAppointment = ""
+    private var selectAppointment = ""
+
     var persons: MutableList<Person> = mutableListOf()
+    var listAdapter: ListAdapter? = null
 
     private val role = mutableListOf(
         "appointment",
@@ -40,6 +44,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var personAgeET: EditText
     private lateinit var spinner: Spinner
     private lateinit var saveBTN: Button
+    private lateinit var autoCompleteTV: AutoCompleteTextView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,37 +58,48 @@ class MainActivity : AppCompatActivity() {
 
         saveBTN.setOnClickListener{
             createPerson()
-            val listAdapter = ListAdapter(this@MainActivity, persons)
+            listAdapter = ListAdapter(this@MainActivity, persons)
             listViewLV.adapter = listAdapter
-            listAdapter.notifyDataSetChanged()
+            listAdapter?.notifyDataSetChanged()
             clearEditFields()
         }
 
-        val adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_spinner_item,
-            role
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
+        //adapter Spinner
+        val adapterSpinner = ArrayAdapter(this, android.R.layout.simple_spinner_item, role)
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapterSpinner
 
         val itemSelectedListener: AdapterView.OnItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                     val item = parent?.getItemAtPosition(position) as String
                     personAppointment = item.toString()
                 }
-
                 override  fun onNothingSelected(parent: AdapterView<*>?) {}
             }
-
         spinner.onItemSelectedListener = itemSelectedListener
 
+        //adapter AutoComplete
+        val adapterAutoComplete = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, role)
+        adapterAutoComplete.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        autoCompleteTV.setAdapter(adapterAutoComplete)
+
+        val itemClickListener: AdapterView.OnItemClickListener =
+            object : AdapterView.OnItemClickListener {
+                override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val item = parent?.getItemAtPosition(position) as String
+                    selectAppointment = item.toString()
+
+                    if (selectAppointment == "") listAdapter = ListAdapter(this@MainActivity, persons)
+                    else listAdapter = ListAdapter(this@MainActivity,
+                        persons.filter { it.appointment == selectAppointment }.toMutableList()
+                    )
+
+                    listViewLV.adapter = listAdapter
+                    listAdapter?.notifyDataSetChanged()
+                }
+            }
+        autoCompleteTV.onItemClickListener = itemClickListener
     }
 
     private fun init() {
@@ -93,6 +109,7 @@ class MainActivity : AppCompatActivity() {
         personLastNameET = findViewById(R.id.personLastNameET)
         personAgeET = findViewById(R.id.personAgeET)
         spinner = findViewById(R.id.spinner)
+        autoCompleteTV = findViewById(R.id.autoCompleteTV)
         saveBTN = findViewById(R.id.saveBTN)
     }
 
